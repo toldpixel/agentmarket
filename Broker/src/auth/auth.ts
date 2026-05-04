c91a8d80a0841357363a382d8f5903442d42f526
toldpixel <software.nutshell@gmail.com>
2026-05-04 20:48:50 +0200

    update: oauth server.ts and auth.ts

--- Broker/src/auth/auth.ts
+++ Broker/src/auth/auth.ts
@@ -21,7 +21,7 @@ const CONFIG = {
 
 function createOAuthUrls() {
   const authBaseUrl = new URL(
-    `https://${CONFIG.auth.host}:${CONFIG.auth.port}/realms/${CONFIG.auth.realm}/`,
+    `https://${CONFIG.auth.host}/realms/${CONFIG.auth.realm}/`,
   );
   return {
     issuer: authBaseUrl.toString(),
